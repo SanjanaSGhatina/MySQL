8796fdc90786daaf198dabbc4eb2752fa107a646
SanjanaSGhatina <114135590+SanjanaSGhatina@users.noreply.github.com>
2023-01-18 21:34:47 +0530

    jan 8 task

---
+++ Cricket.sql
@@ -0,0 +1,42 @@
+create database cricketTeam;
+
+use cricketTeam;
+
+create table cricket(
+id int,
+playername varchar(30),
+country varchar(30),
+Hundreds int,
+fifties int,
+econamy int,
+no_of_innings int,
+playing_order int,
+runns int,
+odi_innings int,
+t20_innings int,
+test_innings int,
+odi_runs int,
+t20_runs int,
+test_runs int);
+
+insert into cricket values(1,'K.L Rahul','India',20,134,134,123,1,1345,43,75,55,4009,4363,2324);
+insert into cricket values(2,'Rohit sharma','India',40,123,154,223,2,3345,73,125,65,8809,6363,3324);
+insert into cricket values(3,'Rishab panth','India',28,184,134,223,5,2345,53,85,55,5009,4363,5324);
+insert into cricket values(4,'M.S Dhoni','India',78,534,344,229,5,1845,73,95,35,7009,6363,7324);
+insert into cricket values(5,'viart kohili','India',71,134,87,223,3,213,87,78,95,10009,15363,7624);
+insert into cricket values(6,'ravindra jadeja','India',34,334,234,128,7,3345,36,55,65,8666,4346,2454);
+insert into cricket values(7,'sachin tendulkar','india',100,250,234,300,4,23454,223,165,160,15097,8363,8324);
+insert into cricket values(8,'verendra sewahg','India',67,342,134,154,3,2345,35,85,65,12009,8363,5324);
+insert into cricket values(9,'mohamod shammi','India',32,342,344,233,9,342,34,34,23,4344,2163,1224);
+insert into cricket values(10,'surya kumar yadav','India',32,233,23,234,3,1245,32,35,25,1009,2363,1324);
+insert into cricket values(11,'ishan kishan','India',50,234,234,213,2,2345,23,35,35,7009,8363,5324);
+insert into cricket values(12,'Shreys iyer','India',40,334,234,223,4,2345,54,45,85,8009,7363,6324);
+insert into cricket values(13,'Buvaneshar kumar','India',45,334,334,323,8,645,45,54,54,3445,4453,3454);
+insert into cricket values(14,'Shubamn gil','India',23,233,343,132,3,3223,34,23,23,34234,5363,6324);
+insert into cricket values(15,'Axar patil','India',34,345,234,342,6,3453,34,23,34,3409,3363,1324);
+insert into cricket values(16,'Hardik pandya','India',43,345,344,233,7,2345,24,32,23,12009,4363,2324);
+insert into cricket values(17,'arshdeep singh','India',24,334,334,523,34,3345,23,25,35,1009,1363,1324);
+insert into cricket values(18,'kuldeep yadav','India',45,122,124,123,8,345,23,33,63,1024,2363,1642);
+insert into cricket values(19,'mohamodh siraj','India',53,34,56,456,10,1345,43,45,65,344,2132,1232);
+insert into cricket values(20,'yajuvendra chal','India',23,24,65,456,9,1345,53,65,25,423,1233,2224);
+insert into cricket values(21,'moon manu','India',20,134,32,123,1,1345,43,75,55,4009,4233,2324);
